014a032eac5c1881c94b9f8af7b4194208ff4c8c
Alexandru <aual@gft.com>
2019-08-03 10:59:56 +0200

    first commit

---
+++ HomeLoginSwift/HomeLoginSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeLoginSwift
+//
+//  Created by Apostol, Alexandru on 03/08/2019.
+//  Copyright © 2019 Apostol, Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
